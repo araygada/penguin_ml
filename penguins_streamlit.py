5681855aba175df5f1d5086c757488e9e3cf5073
araygada <albertoraygada@gmail.com>
2025-06-20 21:11:00 -0600

    Inclusión del secret password

--- penguins_streamlit.py
+++ penguins_streamlit.py
@@ -3,11 +3,19 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 import pickle
+
 st.title('Penguin Classifier: A Machine Learning App')
 st.write("This app uses 6 inputs to predict the species of penguin using "
          "a model built on the Palmer Penguins dataset. Use the form below"
          " to get started!")
-penguin_df = pd.read_csv('penguins.csv')
+
+password_guess = st.text_input("What is the Password?")
+if password_guess != st.secrets['password']:
+    st.stop()
+
+penguin_file = st.file_uploader('Upload your own penguin data')
+
+penguin_df = pd.read_csv(penguin_file)
 rf_pickle = open('random_forest_penguin.pickle', 'rb')
 map_pickle = open('output_penguin.pickle', 'rb')
 rfc = pickle.load(rf_pickle)
